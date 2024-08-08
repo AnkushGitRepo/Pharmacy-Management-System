32f8ab594b88af6a00d3e03706055f52c5948e0b
Ankush Gupta <70625252+AnkushGitRepo@users.noreply.github.com>
2024-08-08 20:20:17 +0530

    Update Customer.java

--- Pharmacy_Management_System/src/main/java/org/example/Customer.java
+++ Pharmacy_Management_System/src/main/java/org/example/Customer.java
@@ -31,16 +31,8 @@ public class Customer {
         this.name = name;
     }
 
-    public String getAddress() {
-        return address;
-    }
-
     public void setAddress(String address) {
         this.address = address;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
     }
 
     public void setPhoneNumber(String phoneNumber) {
